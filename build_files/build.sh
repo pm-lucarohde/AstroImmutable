24219384f315adcb69b1beb362361be6104547ee
pm-lucarohde <luca2001.tlr@gmail.com>
2026-03-11 03:15:50 +0100

    Install and configure Distrobox

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,12 +46,22 @@ dnf5 install -y \
 	7zip\
 	podman\
 	spotify\
-	nautilus
+	nautilus\
+	gnome-software
 
 if flatpak --system remotes | awk '{print $1}' | grep -qx fedora; then
     flatpak --system remote-delete fedora --force
 fi
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+distrobox create --image ubuntu:25.10 --name ubuntu
+distrobox enter ubuntu
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update
+sudo apt install brave-browser
+exit
 
 # Use a COPR Example:
 #
