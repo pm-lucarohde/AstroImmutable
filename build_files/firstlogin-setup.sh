dbda32a716c9ec29c38a49a685c5f03eb635af7e
pm-lucarohde <luca2001.tlr@gmail.com>
2026-05-08 22:21:31 +0200

    Login

--- build_files/firstlogin-setup.sh
+++ build_files/firstlogin-setup.sh
@@ -15,19 +15,17 @@ mkdir -p "${STATE_DIR}"
 kwriteconfig6 --file kdeglobals --group General --key TerminalService com.mitchellh.ghostty.desktop
 
 curl -fL "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak" -o /tmp/hytale.flatpak
+flatpak install --system -y "/tmp/hytale.flatpak"
+rm -rf /tmp/hytale.flatpak
 
 # Flatpaks installieren
-flatpak install --system -y\
+flatpak install -y\
         com.spotify.Client\
         com.ktechpit.whatsie\
         dev.vencord.Vesktop\
         org.mozilla.Thunderbird\
         org.mozilla.firefox\
 		it.mijorus.gearlever
-
-flatpak install --system -y "/tmp/hytale.flatpak"
-
-rm -rf /tmp/hytale.flatpak
 
 # Status-Datei anlegen, damit es beim nächsten Login übersprungen wird
 touch "$STATE_FILE"
